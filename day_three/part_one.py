0c5e534a63580ee41b6084e2e3e0af54038cdc69
idjaw <w.hawari@gmail.com>
2016-12-06 17:06:30 -0500

    more stuff

--- day_three/part_one.py
+++ day_three/part_one.py
@@ -5,7 +5,7 @@ def get_data(file_name):
     with open(file_name) as f:
         return [
             list(map(int, data.strip().split()))
-            for data in f.readlines() if data.strip()
+            for data in f.readlines() if data != '\s'
         ]
 
 if __name__ == '__main__':
