3e00aff5dd7879b54116ae08bf55905bd80d773b
idjaw <w.hawari@gmail.com>
2016-12-07 22:33:44 -0500

    Day 4 Solution Part 1

---
+++ day_four/part_one.py
@@ -0,0 +1,37 @@
+from collections import Counter, defaultdict
+
+
+def get_data(filename):
+    with open(filename) as f:
+        data = f.read().strip().split()
+    return [
+        (
+            i[:-10].replace('-', ''),
+            int(i.split('-')[-1].split('[')[0]),
+            i[-7:][1:-1]
+        ) for i in data
+        ]
+
+
+def get_most_common(data):
+    d = defaultdict(list)
+    for k, v in Counter(data).items():
+        d[v].append(k)
+    return ''.join(
+        ''.join(sorted(v)) for _, v in sorted(d.items(), reverse=True)
+    )[:5]
+
+
+def sum_of_valid_sector_ids(filename):
+    data = get_data('inputs.txt')
+    valid_sectors = 0
+    for name, sector_id, checksum in data:
+        most_common = get_most_common(name)
+        if most_common == checksum:
+            valid_sectors += sector_id
+    return valid_sectors
+
+
+if __name__ == '__main__':
+    print(sum_of_valid_sector_ids('inputs.txt'))
+    # 137896
---
+++ tests/day_four/tests_part_one.py
@@ -0,0 +1,55 @@
+from textwrap import dedent
+from unittest import TestCase
+from unittest.mock import patch, mock_open
+
+from day_four.part_one import get_data, get_most_common, sum_of_valid_sector_ids
+
+mock_inputs = """
+vxupkizork-sgmtkzoi-pkrrehkgt-zxgototm-644[kotgr]
+mbiyqoxsm-pvygob-nocsqx-900[obmqs]
+"""
+
+
+class TestGetData(TestCase):
+
+    @patch('builtins.open', mock_open(read_data=mock_inputs))
+    def test_get_data(self):
+        self.assertEqual(
+            [
+                ("vxupkizorksgmtkzoipkrrehkgtzxgototm", 644, "kotgr"),
+                ("mbiyqoxsmpvygobnocsqx", 900, "obmqs")
+            ],
+            get_data('some_file')
+
+        )
+
+
+class TestGetMostCommon(TestCase):
+    def test_get_most_common(self):
+        test_input = "aaaaabbbzyx"
+        res = get_most_common(test_input)
+        self.assertEqual('abxyz', res)
+
+    def test_get_most_common_2(self):
+        test_input = "totallyrealroom"
+        res = get_most_common(test_input)
+        self.assertEqual('loart', res)
+
+mock_data = dedent("""
+        aaaa-bbb-z-y-x-123[abxyz]
+        a-b-c-d-e-f-g-h-987[abcde]
+        not-a-real-room-404[oarel]
+        totally-real-room-200[decoy]
+        """)
+
+
+class TestDecoding(TestCase):
+    def test_is_real(self):
+        self.assertTrue(get_most_common("aaaaabbbzyx") == "abxyz")
+        self.assertTrue(get_most_common("abcdefgh") == "abcde")
+        self.assertTrue(get_most_common("notarealroom") == "oarel")
+        self.assertFalse(get_most_common("totallyrealroom") == "decoy")
+
+    @patch('builtins.open', mock_open(read_data=mock_data))
+    def test_1(self):
+        self.assertEqual(sum_of_valid_sector_ids('meh'), 1514)
