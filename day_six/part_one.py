7d5a9e3295dfcc26c90a684583efc8ab221b0357
idjaw <w.hawari@gmail.com>
2016-12-08 13:23:55 -0500

    minor refactor

--- day_six/part_one.py
+++ day_six/part_one.py
@@ -3,11 +3,11 @@ from collections import Counter
 
 def get_code(data, get_highest=True):
     code = []
-    gh = 0 if get_highest else -1
+    idx = 0 if get_highest else -1
     with open(data) as f:
         file_data = f.readlines()
     for line in list(map(list, zip(*file_data)))[:-1]:
-        code.append(Counter(''.join(line).strip()).most_common()[gh][0])
+        code.append(Counter(''.join(line).strip()).most_common()[idx][0])
     return ''.join(code)
 
 if __name__ == '__main__':
